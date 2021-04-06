76d305a6b33ccb93bdb80f8caf9206a078f19927
Javier Lizarraga <javierlizarraga.b@hotmail.com>
2021-04-06 16:27:32 -0700

    Organized files

--- server/index.js
+++ server/index.js
@@ -2,19 +2,28 @@ const express = require ("express");
 const app = express();
 
 const mysql = require('mysql');
-const db = mysql.createPool({
+
+var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'bancomer',
+    database: 'bancomer'
 });
 
-const sqlInsert = "INSERT INTO cliente (Num_Cliente, Nombre_Propietario, Fecha_vigencia, Pais) VALUES (6, 'Javo Javito', '2222-03-04', 'MEXICO')"
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+const sqlInsert = "INSERT INTO cliente (Num_Cliente, Nombre_Propietario, Fecha_vigencia, Pais) VALUES (7, 'Javo Javito', '2222-03-04', 'MEXICO')"
+
+
 
 app.get("/Saludo", (req, res) => {
-    db.query(sqlInsert, (err, result)=> (
-        res.send("Hola chavos !!")
-    ))
+    db.query(sqlInsert, function (err, result) {
+        if (err) throw err;
+        console.log("Result: " + result);
+    });
     
 });
 
