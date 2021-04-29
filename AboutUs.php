6d82e283352a37938f77d267180c89012a9fa75f
Hassan Sharif Abbud González <hassanabbud@hotmail.com>
2021-04-28 18:33:31 -0600

    Fotos about us

--- AboutUs.php
+++ AboutUs.php
@@ -46,12 +46,12 @@
         <div class="container">
             <div class="intro">
                 <h2 class="text-center">Equipo de trabajo</h2>
-                <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>
+                <p class="text-center">El equipo está conformado por estudiantes del tecnológico de monterrey de la carrera de ingeniería en tecnologías computacionales. </p>
             </div>
             <div class="row projects">
                 <div class="col-sm-6 item">
                     <div class="row">
-                        <div class="col-md-12 col-lg-5"><a href="#"><img class="img-fluid" src="assets/img/b.jpg"></a></div>
+                        <div class="col-md-12 col-lg-5"><a href="#"><img class="img-fluid" src="assets/img/H.jpg"></a></div>
                         <div class="col">
                             <h3 class="name">Hassan Sharif Abbud González</h3>
                             <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida.</p>
@@ -60,7 +60,7 @@
                 </div>
                 <div class="col-sm-6 item">
                     <div class="row">
-                        <div class="col-md-12 col-lg-5"><a href="#"><img class="img-fluid" src="assets/img/b.jpg"></a></div>
+                        <div class="col-md-12 col-lg-5"><a href="#"><img class="img-fluid" src="assets/img/j.jpg"></a></div>
                         <div class="col">
                             <h3 class="name">Javier Lizárraga Beyles<br></h3>
                             <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida.</p>
@@ -69,7 +69,7 @@
                 </div>
                 <div class="col-sm-6 item">
                     <div class="row">
-                        <div class="col-md-12 col-lg-5"><a href="#"><img class="img-fluid" src="assets/img/b.jpg"></a></div>
+                        <div class="col-md-12 col-lg-5"><a href="#"><img class="img-fluid" src="assets/img/A.jpg"></a></div>
                         <div class="col">
                             <h3 class="name">Jesús Armando Valdez Mendívil<br></h3>
                             <p class="description">Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida.</p>
